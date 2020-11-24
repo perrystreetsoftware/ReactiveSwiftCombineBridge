3f9edd97be63d4a3058a19c80315b46cd4ecd3a4
Bruno Campos <camposbrunocampos@gmail.com>
2020-11-23 21:17:33 -0300

    Adding combine bridge to the repo

---
+++ Tests/ReactiveSwiftCombineBridgeTests/ReactiveSwiftTests.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eric Silverberg on 11/17/20.
+//
+
+import Foundation
+import ReactiveSwift
+import Combine
+import XCTest
+import CombineExpectations
+@testable import ReactiveSwiftCombineBridge
+
+final class ReactiveSwiftTests: XCTestCase {
+    func testSubscribeExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        let producer = SignalProducer<Int, Never> { (observer, _) in
+            observer.send(value: 1)
+            observer.sendCompleted()
+        }
+
+        let recorder = producer.publisher().record()
+        let nextInt = try? recorder.next().get()
+
+        XCTAssertEqual(nextInt, 1)
+    }
+}
